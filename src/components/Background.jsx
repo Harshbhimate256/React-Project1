ef6d007a157871f07d3abb352a2e7d21556ef773
Harsh Bhimate <harshbhimate2018@gmail.com>
2024-01-25 23:04:35 +0530

    added 2 components bg and fg

---
+++ src/components/Background.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <>
+        <div className='bg-main'>
+        <div className='nav'>Documents</div>
+        <h1 className='docs'>Docs</h1>
+        </div>
+    </>
+  )
+}
+
+export default Background
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Foreground() {
+  return (
+    
+      <div className='fr-ground'></div>
+  )
+}
+
+export default Foreground
